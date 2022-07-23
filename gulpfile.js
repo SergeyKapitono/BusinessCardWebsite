c44907d84c7f649fc18824f25dc43afbd5ba40fd
Sergey Kapitonov <k956646@gmail.com>
2022-07-23 17:51:29 +0300

    gulp2

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,36 @@
 import gulp from "gulp";
 // Импорт путей
 import { path } from "./gulp/config/path.js";
+// Импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
 // Передаем значение в глобальную переменную
 global.app = {
      path: path,
-     gulp: gulp
+     gulp: gulp,
+     plugins: plugins
 }
 
 // Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
 
 
 // Наблюдатель за изменения в файлах
 function watcher() {
-    gulp.watch(path.watch.files, copy)
-    gulp.watch(path.watch.html, html)
-
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);
+// Оснавные задачи
+const mainTasks = gulp.parallel(copy, html, sass);
 
 // Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks,gulp.parallel(watcher, server));
 
 // Выполнение сценария по умолчанию
 gulp.task('default', dev); 
